c71026671c9aac639fe9db1a34f54dd6875d91e1
lschuenemann <lennardo1996@gmail.com>
2025-03-24 13:35:02 +0100

    added script to convert glb to obj meshes since glb is incompatible with pybullet

---
+++ articulate_anything/glb_to_obj.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# input_dir="/home/link/DreMa/third_party/articulate-anything/datasets/partnet-mobility-v0/dataset/generated_object"
+input_dir="/home/link/DreMa/third_party/articulate-anything/datasets/segmentation_masks/drawer_multi-view/output/parts"
+
+for file in "$input_dir"/*.glb; do
+  base_name=$(basename "$file" .glb)
+  assimp export "$file" "$input_dir/$base_name.obj"
+done
+
+echo "Conversion finished! Files saved in $input_dir."
