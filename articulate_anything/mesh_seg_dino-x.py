26df68d8957c33bb5ed3773273b852c6ea18898a
lschuenemann <lennardo1996@gmail.com>
2025-03-24 13:33:19 +0100

    updated segmentation

--- articulate_anything/mesh_seg_dino-x.py
+++ articulate_anything/mesh_seg_dino-x.py
@@ -128,7 +128,7 @@ if __name__ == "__main__":
     # text_prompt = "top surface.  leg.  frame.  handle.  drawer." # drawer
     text_prompt = "display window.  main body/housing.  control panel.  handle.  door." # microwave
      # laptop
-    api_token = "1c9a49043bb8d909075526295bb20fc4"#os.environ.get("DINO_X_API_KEY")
+    api_token = os.environ.get("DINO_X_API_KEY") #"1c9a49043bb8d909075526295bb20fc4"
     
     
     process_multiple_views(input_dir, text_prompt, output_dir, api_token)
